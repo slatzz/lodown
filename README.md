e76131c958412777ecc4c834c4a9c076f193922b
Ubuntu <ubuntu@ip-172-31-55-129.ec2.internal>
2020-06-18 00:22:51 +0000

    Update lodown pseudocode

--- README.md
+++ README.md
@@ -1,121 +1,14 @@
-# lodown
-An npm functional programming library project
+         ___        ______     ____ _                 _  ___  
+        / \ \      / / ___|   / ___| | ___  _   _  __| |/ _ \ 
+       / _ \ \ /\ / /\___ \  | |   | |/ _ \| | | |/ _` | (_) |
+      / ___ \ V  V /  ___) | | |___| | (_) | |_| | (_| |\__, |
+     /_/   \_\_/\_/  |____/   \____|_|\___/ \__,_|\__,_|  /_/ 
+ ----------------------------------------------------------------- 
 
-In this project, you're going to create your own functional library of code and release this code for use through the `npm` (Node Package Management) platform.
 
-You'll then use your lodown library to solve problems in the next project.
+Hi there! Welcome to AWS Cloud9!
 
-## Setup
-1. Fork this repository to your own GitHub account.
-2. Clone your forked version of the lodown repository to a new Cloud9 workspace.
-3. We'll need a `package.json` file, which describes for `npm` our library project and lists any dependant libraries of code. To create a `package.json` configuration file for our lodown library, from the command-line, run the command:
-    
-        npm init
-4. **IMPORTANT CONFIGURATION**: Fill in the prompts with your own details:
-    
-    1. When asked to name your lodown project, give it a unique name by appending your GitHub username to the name of the project (of course, replacing `<my-github-username>` with your actual GitHub username.), like so:
-    
-        `lodown-<my-github-username>`
+To get started, create some files, play with the terminal,
+or visit https://docs.aws.amazon.com/console/cloud9/ for our documentation.
 
-    2. When asked for **test command**, enter:
-        
-        `istanbul cover _mocha -- test/ -R spec`
-
-    3. When asked for your **git repository**, you will probably be given the correct url as the default, so just select this by pressing `return`, otherwise, provide **your forked GitHub repository's url**.
-    
-5. Once you're done creating your `package.json` file, go to the command line and install the required test libraries by running the following command:
-    
-        npm install i -D mocha chai sinon istanbul
-
-6. Implement your library in the file `index.js`. Remember that `index.js` is a Node.js module, so you must export your API using `module.exports`, for example:
-    
-        function each(collection, action) {
-            // ...code
-        }
-        module.exports.each = each;
-
-7. Write clear docs for each Function using the proper JavaScript documentation format. You should clearly explain how each Function works, the type of each paramater and how each paramater is used, and explain any return value. It should be evident from your docs you clearly understand how each Function works. Bonus points for listing usage examples in your docs. Example:
-    
-        /**
-         * filter: Designed to filter values in a collection based on a test. 
-         * Takes a collection, Array or Object, and passes each value 
-         * in the collection through a test Function. The test Function returns 
-         * true if the value passes the test, false otherwise. Values that pass 
-         * the test are collected and returned in an output Array.
-         * 
-         * @param {Array or Object} collection: The collection to filter.
-         * @param {Function} test: The Function to be applied to each value in 
-         * the collection. The test Function must return a Boolean based on some 
-         * logic which tests the value given to it.
-         * 
-         * @return {Array}: An Array containing the filtered collection values. 
-         * The Array will contain only the values that passed the test.
-         * 
-         * Usage: 
-         * 
-         *      const letters = ['a', 'b', 'b', 'c'];
-         *      const onlyBs = filter(letters, function(letter) {
-         *          return letter === 'b';
-         *      });
-         *      console.log(onlyBs); // -> ['b', 'b']
-         */
-        function filter(collection, test) {
-            // ...code
-        }
-        module.exports.each = each;
-
-## Release your library!
-
-Once you're done with all Functions and their docs, release your lodown library to `npm` (node package manager), following the steps below to do so (using your GitHub username to create a unique name for your library):
-
-1) Create a new account in <a href="https://www.npmjs.com" target="_blank">npm's website</a>.
-2) Run this command from the terminal
-        
-        npm login
-        
-3) Enter your username and password. This will store the credentials so you don’t have to enter it for every publish.
-4) Now to publish, run
-
-        npm publish
-
-This will publish your package to NPM registry. Once publish completes(in less than a minute), you can search for your package on <a href="https://www.npmjs.com" target="_blank">npm's website</a>.
-
-### Updating your library
-
-If you want to make changes to your package, you have to change the version number and publish again.
-
-The easiest way to do so is using the npm commands :
-
-    npm version patch
-    npm version minor 
-    npm version major 
-        
-to update the version automatically rather than manually updating them. These commands are based on <a href="https://docs.npmjs.com/about-semantic-versioning" target="_blank">semantic versioning</a>.
-
-## Optional: Unit tests
-
-If you want to learn about and create unit tests for your code, write your tests in the file `test/index.spec.js`. Check out the <a href="http://chaijs.com/api/bdd/" target="_blank">chai api for examples on asserting expectations</a> and <a href="http://stackoverflow.com/questions/17526805/chai-test-array-equality-doesnt-work-as-expected" target="_blank">be careful when asserting/expecting against complex types</a> - you'll have to make use of the `.eql` api, and not `.equal`. For example:
-    
-        expect(lodown.filter([1, 2, 3], function(value) { return value > 2; } )).to.eql([3]);
-8. To run your tests, run the following command:
-    
-        npm test
-9. Check the console output for passes and failures.
-10. Preview (Cloud9) or open the file at `coverage/lcov-report/index.html` for your coverage report. Make certain all lines of code are traversed in your tests!
-
-### Resources:
-
-#### Docs
-
-1. https://mochajs.org
-2. http://chaijs.com/api/bdd/
-3. http://sinonjs.org/docs/#spies-api
-
-#### Reading
-1. http://marcofranssen.nl/using-mocha-chai-sinon-to-test-node-js/
-2. http://stackoverflow.com/questions/19298118/what-is-the-role-of-describe-in-mocha
-3. http://www.vapidspace.com/coding/2014/10/29/code-coverage-metrics-with-mocha-and-istanbul/
-
-### Quick Videos
-1. https://egghead.io/lessons/javascript-how-to-write-a-javascript-library-setting-up-unit-testing-with-mocha-and-chai
-2. https://egghead.io/lessons/javascript-how-to-write-a-javascript-library-unit-testing-with-mocha-and-chai
+Happy coding!
--- index.js
+++ index.js
@@ -3,12 +3,190 @@
 // YOU KNOW WHAT TO DO //
 
 /**
+ * 1. identity
+ * 
+ * Designed to return the given input value.
+ * 
+ * @param: {Any value}, any datatype.
+ * 
+ * @return: {Any value}, the given input value, unchanged.
+ * 
+ */
+ 
+ function identity(value){
+    return value;
+};
+
+module.exports.identity = identity;
+
+/**
+ * 2. typeOf
+ * 
+ * Designed to take an input value and return its datatype as a string.
+ * 
+ * @param: {Any value}, any datatype.
+ * 
+ * @return: {String}, the input value's datatype returned as a string.
+ * 
+ */
+ 
+function typeOf(value){
+
+    let type = typeof(value);
+    
+    if(type == "object"){ 
+        
+        if(Array.isArray(value)){ 
+            type = "array";
+            return type;
+            
+        } else if(value === null){
+            type = "null";
+            return type;
+        }
+    }
+         return type;
+    };
+
+module.exports.typeOf = typeOf;
+
+
+
+/**
+ * 3. first
+ * 
+ * Designed to return the first number of elements as specified by a given input number. If the array is not an array, 
+ * or if the number is negative, an empty array will be returned. If the array is empty, the first element of the array
+ * will be returned. If the number is greater than the length of the array, the whole array will be returned.
+ * 
+ * @param: {Array}, [].
+ * 
+ * @param: {Number}, any number.
+ * 
+ * @return: {Any value}, the first number of specified elements, which can be any datatype.
+ * 
+ */
+ 
+  function first(array, number){
+     
+     if(!Array.isArray(array) || number <= 0){
+         return [];
+     } 
+    
+     let elements = [];
+
+     for(let i = 0; i < array.length && i < number; i++){ 
+         elements.push(array[i]);
+     } 
+     
+     if(elements.length === 0){ 
+         elements = array[0]; 
+     }
+     return elements;
+ };
+
+module.exports.first = first;
+
+
+/**
+ * 4. last
+ * 
+ * Designed to return a specified number of the last elements of the array. If the array is not an array or if the 
+ * number is negative, an empty array will be returned. If the number is greater than the length of the array, the 
+ * whole array is returned. If the array is empty or a number is not given, the last element of the array will be
+ * returned.
+ * 
+ * @param: {Array}, [].
+ * 
+ * @param: {Number}, any number.
+ * 
+ * @return: {Any value}, the last number of specified elements, which can be any datatype.
+ */
+ 
+ function last(array, number){
+   
+    let lastElements = [];
+    let i;
+    
+    if(!Array.isArray(array) || number <= 0){
+        return [];
+    } 
+
+    number > array.length ? i = 0: i = array.length - number;
+
+    for(i; i < array.length; i++){ 
+        lastElements.push(array[i]);
+    }
+    if(lastElements.length === 0 || isNaN(number)){
+        return array[array.length - 1];
+    }   
+        return lastElements;
+    };
+
+ module.exports.last = last;
+
+
+
+/**
+ * 5. indexOf
+ * 
+ * Designed to return the numerical index of a specified value within an array. If the value occurs multiple times in the array,
+ * the index of its first occurrence will be returned. If the value is not found within the array, -1 will be 
+ * returned.
+ * 
+ * @param: {Array}, [].
+ * 
+ * @param: {Any value}, value can be any datatype.
+ * 
+ * @return: {Number}, number representing the index of the given value. 
+ * 
+ */
+ 
+ function indexOf(array, value){
+    
+  for(let i = 0; i < array.length; i++){
+            if(array[i] === value){
+                return i;
+            }   
+        }
+        return -1;
+    };
+    
+module.exports.indexOf = indexOf;
+
+
+/**
+ * 6. contains
+ * 
+ * Designed to return a Boolean value. If a given array contains a given value, will return true. Will return
+ * false, otherwise. 
+ * 
+ * @param: {Array}, [].
+ * 
+ * @param: {Any value}, can be any datatype.
+ * 
+ * @return: {Boolean}, will return either true or false.
+ * 
+ */
+ 
+ function contains(array, value){
+   return array.includes(value) ? true: false;
+};
+
+module.exports.contains = contains;
+
+
+/**
+ * 7. each
+ * 
  * each: Designed to loop over a collection, Array or Object, and applies the 
  * action Function to each value in the collection.
  * 
  * @param {Array or Object} collection: The collection over which to iterate.
+ * 
  * @param {Function} action: The Function to be applied to each value in the 
  * collection
+ * 
  */
 function each(collection, action) {
     if(Array.isArray(collection)) {
@@ -22,3 +200,314 @@ function each(collection, action) {
     }
 }
 module.exports.each = each;
+
+
+
+/**
+ * 8. unique
+ * 
+ * Designed to remove all duplicates within an array and return a new array with only unique values.
+ * 
+ * @param: {Array}, [].
+ * 
+ * @return: {A new array}, [], with unique values and all duplicates removed.
+ * 
+ */
+ 
+ function unique(array){
+    
+        let uniqueArray = [];
+ 
+    for(let i = 0; i < array.length; i++){
+        if(!uniqueArray.includes(array[i]) && (indexOf(array, array[i]) !== -1)){
+            uniqueArray.push(array[i]);
+        } 
+    }
+   
+    return uniqueArray;
+};
+module.exports.unique = unique;
+
+
+
+/**
+ * 9. filter
+ * 
+ * Designed to take an array and pass a function over its elements. All elements that evaluate to true
+ * after the function passes over it are pushed into a new array with all truthy values. 
+ * 
+ * @param: {Array}, [].
+ * 
+ * @param: {Function}, a function that will perform an action on the array's elements.
+ * 
+ * @return: {Array}, [], a new array with all values that returned true.
+ * 
+ */
+ 
+ function filter(array, action){
+
+let filtered = [];
+
+each(array, function(element, index, array){
+  
+    if(action(element, index, array)){
+     filtered.push(element);
+    }
+});
+return filtered;
+};
+module.exports.filter = filter;
+
+
+
+/**
+ * 10. reject
+ * 
+ * Designed to take an array and pass a function over all of its elements. If the elements result to false after
+ * the function has passed over them, then they are pushed into a new array with all other elements that returned
+ * false.
+ * 
+ * @param: {Array}, [].
+ * 
+ * @param: {Function}, a function that will perform an action on the array's elements.
+ * 
+ * @return: {Array}, [], a new array with all elements that returned false.
+ * 
+ */
+ 
+ function reject(array, action){
+    
+let rejects = [];
+
+each(array, function(element, index, array){
+   
+    if(!action(element, index, array)){
+     rejects.push(element);
+    }
+});
+
+return rejects;
+};
+module.exports.reject = reject;
+
+
+
+/**
+ * 11. partition
+ * 
+ * Designed to take an array and a function. Will pass the function over all the elements in the array and push
+ * the elements that returned true into a new array, will push all the elements that returned false into a separate 
+ * array, and will push both of the subarrays into a final array in which they are contained.
+ * 
+ * @param: {Array}, [].
+ * 
+ * @param: {Function}, a function that will perform an action on the array's elements.
+ * 
+ * @return: {Array}, [], will return an array containing two subarrays.
+ * 
+ */
+ 
+function partition(array, action){
+    
+let finalArray = [];
+let truthyArray = [];
+let falseyArray = [];
+
+each(array, function(element, index, array){
+    action(element, index, array) ? truthyArray.push(element) : falseyArray.push(element);
+});
+
+finalArray.push(truthyArray, falseyArray);
+
+return finalArray;
+};
+module.exports.partition = partition;
+
+
+
+
+/**
+ * 12. map
+ * 
+ * Designed to take in a collection (array or object) and pass a function over its contents. Will push all of the 
+ * modified elements/properties into a new array once the function has been called over all of its contents.
+ * 
+ * @param: {Collection}, can be any array, [], or object, {}.
+ * 
+ * @param: {Function}, a function that will perform an action on the collection's elements/properties.
+ * 
+ * @return: {Array}, [], will return an array with the modified elements/properties.
+ * 
+ */ 
+ 
+function map(collection, action){
+
+let mapArray = [];
+
+if(Array.isArray(collection)){ 
+    for(let i = 0; i < collection.length; i++){
+        mapArray.push(action(collection[i], i, collection));
+} 
+} else if(typeOf(collection) === "object"){
+    for(let key in collection){ 
+        mapArray.push(action(collection[key], key, collection));
+    }
+} 
+return mapArray;
+
+};
+module.exports.map = map;
+
+
+
+/**
+ * 13. pluck
+ * 
+ * Designed to map over an array containing objects and return all of its values as strings within a new array.
+ * 
+ * @param: {Array}, [], an array containing objects.
+ * 
+ * @param: {Property}, a key property represented as a string.
+ * 
+ * @return: {Array}, [], a new array containing the object's values represented as strings.
+ * 
+ */
+ 
+ function pluck(arrayOfObjs, prop){
+    
+    let plucked = [];
+    
+       map(arrayOfObjs, function(objInArr, i, arrayOfObjs){
+                plucked.push(arrayOfObjs[i][prop]);
+        });
+        
+        return plucked;
+};
+module.exports.pluck = pluck;
+
+
+/**
+ * 14. every
+ * 
+ * Designed to call a function over a collection's contents and return true if every element/property returned
+ * true after the function is called, or if there are truthy results and no function is given. Will return false if at least one element/property 
+ * returns false or if there are falsey results and no function is given.
+ * 
+ * @param: {Collection}, can be any array, [], or object, {}.
+ * 
+ * @param: {Function}, a function that will perform an action on the collection's contents.
+ * 
+ * @return: {Boolean}, returns either true or false.
+ * 
+ */
+ 
+ function every(collection, action){
+    
+    let result = true;
+    
+   each(collection, function(element, index, collection){
+       
+       if(arguments.length > 1 && typeOf(action) === "function"){
+           !action(element, index, collection) ? result = false : result;
+       } 
+            else { 
+                !element ? result = false : result;
+            }
+       });
+            return result;
+};
+module.exports.every = every;
+
+
+
+/**
+ * 15. some
+ * 
+ * Designed to call a function over a collection's contents and return true if at least one element/property returned
+ * true after the function is called, or if there are truthy results and no function is given. Will return false when no element/property 
+ * returns true or if there are falsey results and no function is given.
+ * 
+ * @param: {Collection}, can be any array, [], or object, {}.
+ * 
+ * @param: {Function}, a function that will perform an action on the collection's contents.
+ * 
+ * @return: {Boolean}, returns either true or false.
+ * 
+ */
+ 
+ function some(collection, action){
+    
+    let result = false;
+    
+    each(collection, function(element, index, collection){
+       
+       if(arguments.length > 1 && typeOf(action) === "function"){
+           action(element, index, collection) ? result = true : result;
+       } 
+            else { 
+                element ? result = true : result;
+            }
+       });
+            return result;
+};
+module.exports.some = some;
+
+
+/**
+ * 16. reduce
+ * 
+ * Designed to take an array, a function, and a seed. Will pass the function over the elements in the array
+ * and return the result of the function call. If no seed is given, the seed will be assigned to the first element
+ * of the array.
+ * 
+ * @param: {Array}, [].
+ * 
+ * @param: {Function}, a function that will perform an action on the collection's contents.
+ * 
+ * @param: {Seed/Any value}, seed can be any value and datatype.
+ * 
+ * @return: {Any value}, will return the result of the function call upon the array's elements. 
+ * 
+ */
+ 
+ function reduce(array, action, seed = array[0]){ 
+    let result = seed;
+   
+    let i;
+        
+        for(arguments.length === 3 ? i = 0 : i = 1; i < array.length; i++){
+            result = action(result, array[i], i);
+        } 
+    return result;
+};
+module.exports.reduce = reduce;
+
+
+/**
+ * 17. extend
+ * 
+ * Designed to take in multiple objects and update the first object with the other provided objects' properties.
+ * 
+ * @param: {Object}, {}.
+ * 
+ * @param: {Objects}, any specified number of other objects.
+ * 
+ * @return: {Object}, {}, the first object, updated with the other objects' properties.
+ * 
+ */
+ 
+ function extend(object, ...objects){ 
+    
+    for(let i = 0; i < objects.length; i++){ 
+        
+        let currObj = objects[i];
+        
+        for(let key in currObj){ 
+            object[key] = currObj[key]; 
+        }
+    }
+    
+    return object;
+    
+};
+module.exports.extend = extend;
